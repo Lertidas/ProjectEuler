65ceecd8f5dd275d172e3739c3888ca782f76802
Lertidas <lucas.schober@gmail.com>
2022-05-01 14:54:15 -0500

    Added Timer

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
         System.out.println("Working");
+        long endTime = System.currentTimeMillis();
+        System.out.println((endTime - startTime) / 1000.0 + " seconds");
     }
 }
